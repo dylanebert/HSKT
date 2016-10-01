7c373e396ea0d866cf36ff836700f8dff5e64c89
dylanebert <dylan.ebert@gmail.com>
2016-10-01 15:06:36 -0500

    Added participant report

--- analysis.js
+++ analysis.js
@@ -9,23 +9,25 @@ $(document).ready(function() {
 	var seqDict = ['123434', '3423132412', '1324323123', '4313122423'];
 	var partDict = ['Head', 'Shoulders', 'Knees', 'Toes'];
 	
-	var playing, timer, startTime, seqPos, curStep, i, j, k;
+	var playing, timer, startTime, seqPos, curStep, i, j;
 	var maxTime = 3000; //Time (ms) between each command
 	var buffers = [];
-	playing = timer = startTime = seqPos = curStep = i = k = 0;
+	playing = timer = startTime = seqPos = curStep = i = 0;
 	j = -2;
 
 	//Declare canvases
 	var playbackCanvas = document.getElementById('playback');
 	var predictionCanvas = document.getElementById('confidences');
+	var reportCanvas = document.getElementById('report');
 	var pbctx = playbackCanvas.getContext('2d');
 	var pctx = predictionCanvas.getContext('2d');
+	var rctx = reportCanvas.getContext('2d');
 	pbctx.textAlign = 'center';
 	pbctx.font = '16px sans-serif';	
 	var predictionChart = new Chart(pctx, {
 		type: 'doughnut',
 		data: {
-			labels: ["Head", "Shoulders", "Knees", "Toes"],
+			labels: ['Head', 'Shoulders', 'Knees', 'Toes'],
 			datasets: [{
 				data: [0.25, 0.25, 0.25, 0.25],
 				backgroundColor: [
@@ -45,7 +47,46 @@ $(document).ready(function() {
 		options: {
 			responsive: false
 		}
-	});	
+	});
+	var reportChart = new Chart(rctx, {
+		type: 'bar',
+		data: {
+			labels: ['Head', 'Shoulders', 'Knees', 'Toes'],
+			datasets: [{
+				data: [100, 100, 100, 100],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)'
+				],
+				borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)'
+				]
+			}]
+		},
+		options: {
+			title: {
+				display: true,
+				text: 'Average % Correct by Part'
+			},
+			responsive: false,
+			scales: {
+				yAxes:[{
+					ticks: {
+						min: 0,
+						max: 100
+					}
+				}]
+			},
+			legend: {
+				display: false
+			}
+		}
+	});
 	var imageObj = new Image();
 	imageObj.onload = function() {
 		pbctx.save();
@@ -53,48 +94,57 @@ $(document).ready(function() {
 		pbctx.drawImage(this, 0, 0, 1920, 1080, 0, 0, 640, 360);
 		pbctx.restore();
 	};
+	function showInfo(info) {
+		pbctx.clearRect(0, 0, 640, 360);
+		pbctx.fillText(info, playbackCanvas.width / 2, playbackCanvas.height / 2);
+	}
 	
 	//Intialisation
 	$('body').on('mousedown', function(e) {
 		e.preventDefault();
 	});
 	$('.container').fadeIn();
-	pbctx.clearRect(0, 0, 640, 360);
-	pbctx.fillText('Choose recording above', playbackCanvas.width / 2, playbackCanvas.height / 2);
+	showInfo('Choose recording above');
 	$('.progress-bar').css('width','0%').attr('aria-valuenow', 0).text('0%');
 	
 	//Scores modal
 	$('#scores_button').click(function() {
-		$('.modal').modal('show');
+		$('#score_modal').modal('show');
 		if(playing == 1)
 			$('#analysis-play').trigger('click');
 	});
-	$('#modal_close').click(function() {
-		$('.modal').modal('hide');
+	$('#score_modal_close').click(function() {
+		$('#score_modal').modal('hide');
+	});
+	
+	//Report modal
+	$('#report_button').click(function() {
+		$('#report_modal').modal('show');
+		if(playing == 1)
+			$('#analysis-play').trigger('click');
+	});
+	$('#report_modal_close').click(function() {
+		$('#report_modal').modal('hide');
 	});
 	
 	//Populate participants menu
 	fs.readFile('C:/data/participants.csv', 'utf-8', function read(err, data) {
 		if(err)
-			console.log(err.toString());
+			showInfo(err);
 		else {
 			csv.parse(data, function(err, output) {
 				if(err)
-					console.log(err.toString());
+					showInfo(err);
 				else {
 					output.forEach(function(entry) {
 						var valid = false;
-						var toAppend = '';
-						toAppend += '<li class="dropdown-submenu"><a class="test" tabindex="-1" href="#">' + entry[0] + ' ' + '<span class="caret"></span></a><ul class="dropdown-menu">';
+						var k;
 						for(k = 1; k < entry.length; k++) {
-							if(fs.existsSync(entry[k].trim() + 'analysis.csv')) {
-								toAppend += '<li><a href="#" class="participant-entry" path="' + entry[k].trim() + '">' + 'Step ' + k + '</a></li>';
+							if(fs.existsSync(entry[k].trim() + 'analysis.csv') && fs.existsSync(entry[k].trim() + 'score.csv'))
 								valid = true;
-							}
 						}
-						toAppend += '</ul></li>';
 						if(valid)
-							$('#participants-menu').append(toAppend);
+							$('#participants-menu').append('<li><a href="#" class="participant-entry" path="' + entry + '">' + entry[0].trim() + '</a></li>');
 					});
 					enableMenus();
 				}
@@ -102,15 +152,28 @@ $(document).ready(function() {
 		}
 	});
 	function enableMenus() {
-		$('.dropdown-submenu a.test').on('click', function(e) {
-			$(this).next('ul').toggle();
-			e.stopPropagation();
-			e.preventDefault();
-		});
 		$('.participant-entry').on('click', function(e) {
-			pbctx.clearRect(0, 0, 640, 360);
-			pbctx.fillText('Loading...', playbackCanvas.width / 2, playbackCanvas.height / 2);
-			reqVideo($(this).attr('path'));			
+			$('#step-menu').html('');
+			var valid = false;
+			var split = $(this).attr('path').split(',');
+			var k;
+			for(k = 1; k < split.length; k++) {
+				if(fs.existsSync(split[k].trim() + 'analysis.csv') && fs.existsSync(split[k].trim() + 'score.csv')) {
+					valid = true;
+					$('#step-menu').append('<li><a href="#" class="step-entry" path="' + split[k].trim() + '">' + 'Step ' + k + '</a></li>');
+				}
+			}
+			if(valid) {
+				$('#step-button').removeClass('disabled').html('Select Step <span class="caret"></span>');
+				$('#participant-button').html($(this).text() + ' <span class="caret"></span>');
+				setReport($(this).attr('path'));
+				$('.step-entry').on('click', function(e) {
+					$('#step-button').html($(this).text() + ' <span class="caret"></span>');
+					reqVideo($(this).attr('path'));	
+				});
+			} else {
+				$('#step-button').addClass('disabled');
+			}
 		});
 	}
 	
@@ -208,17 +271,27 @@ $(document).ready(function() {
 	}, 30);
 	
 	function reqVideo(dir) {
+		//Reset values
+		showInfo('Loading...');
+		playing = timer = i = k = 0;
+		j = -2;
+		buffers = [];
+		$('.progress').fadeIn();
+		$('.progress-bar').css('width','0%').attr('aria-valuenow', 0).text('0%');
+		$('#scores').html('');
+		
+		//Load video
 		dir = dir.toString().replace(/\\/g, '/');
-		console.log(dir);
 		var lookup = {};
 		fs.readFile(dir + 'analysis.csv', 'utf8', function(err, data) {
 			if(err) {
-				console.log(err.toString());
+				showInfo(err);
+				console.log(dir);
 			}
 			else {
 				csv.parse(data, function(err, output) {
 					if(err) {
-						console.log(err.toString());
+						showInfo(err);
 					}
 					else {
 						output.forEach(function(entry) {
@@ -226,16 +299,15 @@ $(document).ready(function() {
 						});
 						fs.readdir(dir + 'frames/', function(err, files) {
 							if(err) {
-								console.log(err.toString());
+								showInfo(err);
 							}
 							else {
-								var k = 0;
 								next();
 								function next() {
 									if(k < files.length) {
 										fs.readFile(dir + 'frames/' + files[k], function(err, buf) {
 											if(err) {
-												console.log(err.toString());
+												showInfo(err);
 											}
 											else {
 												buffers.push({buffer: buf.toString('base64'), name: files[k].toString(), dataclass: lookup[files[k]][0], confidences: lookup[files[k]][1]});
@@ -247,11 +319,8 @@ $(document).ready(function() {
 														var a = frameTimeHMS.split('-');
 														startTime = parseFloat(a[0] * 3600 + a[1] * 60 + a[2]);
 														$('.analysis-controls').removeClass('noevents').fadeTo(0, 1);
-														playing = 0;
-														timer = 0;
 														var stepString = /step\d/.exec(dir).toString();
 														curStep = parseInt(stepString.charAt(stepString.length - 1)) - 1;
-														j = -2;
 														drawFrame();
 														drawConfidences();
 														setScore(dir);
@@ -275,11 +344,11 @@ $(document).ready(function() {
 	function setScore(dir) {
 		fs.readFile(dir + 'score.csv', 'utf-8', function(err, data) {
 			if(err)
-				console.log(err.toString());
+				showInfo(err);
 			else {
 				csv.parse(data, function(err, output) {
 					if(err)
-						console.log(err.toString());
+						showInfo(err);
 					else {
 						$('#scores_button').removeClass('disabled');
 						output.forEach(function(entry) {							
@@ -289,5 +358,65 @@ $(document).ready(function() {
 				});
 			}
 		});
+	}
+	
+	function setReport(dirs) {
+		var reportData = {};
+		partDict.forEach(function(entry) {
+			reportData[entry] = [];
+		});
+		var k = 1;
+		var valid;
+		function next() {
+			var dir = dirs.split(',')[k].trim();
+			if(dir != 'undefined') {
+				valid = true;
+				fs.readFile(dir + 'score.csv', 'utf-8', function(err, data) {
+					if(err)
+						showInfo(err);
+					else {
+						csv.parse(data, function(err, output) {
+							if(err)
+								showInfo(err);
+							else {
+								output.forEach(function(entry) {
+									reportData[partDict[seqDict[k - 1][parseInt(entry[0].trim()) - 1] - 1]].push(entry[2]);
+								});
+								if(k < 4) {
+									k++;
+									next();
+								} else {
+									calc();
+								}
+							}
+						});
+					}
+				});
+			} else if(k < 4) {
+				k++;
+				next();
+			} else {
+				calc();
+			}
+		}
+		next();
+		function calc() {
+			partDict.forEach(function(entry) {
+				if(reportData[entry].length > 1) {
+					var avg = 0;
+					for(k = 0; k < reportData[entry].length; k++) {
+						avg += parseFloat(reportData[entry][k]);
+					}
+					avg /= reportData[entry].length;
+					reportChart.data.datasets[0].data[partDict.indexOf(entry)] = avg;
+				} else {
+					reportChart.data.datasets[0].data[partDict.indexOf(entry)] = parseFloat(reportData[entry][0]);
+				}
+			});
+			if(valid) {
+				reportChart.update();
+				$('#report_button').removeClass('disabled');
+			}
+		}
 	}
 });
